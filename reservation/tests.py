74a60de27318c49f3ae1ae0ef1f55f92271ee5ca
hrfg <kox77@yahoo.com>
2023-07-10 22:13:21 +0300

    added test

--- reservation/tests.py
+++ reservation/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Menu
 
-# Create your tests here.
+
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title="test item", price=7.77, inventory=3)
+        itemstr = item.__str__()
+
+        self.assertEqual(itemstr, "ID:1 - test item")
